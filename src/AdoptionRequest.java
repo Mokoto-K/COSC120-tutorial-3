fa88b9dc48806748b4fefd9f358c9303b088299d
mokotok <mokotok@protonmail.com>
2024-06-29 16:39:24 +1000

    fourth day - Finished... i think

--- src/AdoptionRequest.java
+++ src/AdoptionRequest.java
@@ -2,11 +2,68 @@ import javax.swing.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class AdoptionRequest {
+
+    public static void main(String[] args) {
+        AdoptionRequest ar = new AdoptionRequest();
+
+        Map<String, Dog> dogDataFromTxt = new HashMap<>();
+        dogDataFromTxt = ar.loadDogData();
+
+        // Set dog with microschip number "989343556" to desexed
+        Dog dog = (Dog) dogDataFromTxt.get("989343556");
+        dog.setDeSexed(true);
+
+        // IMPORTANT BLOCK TO STUDY
+        // Creating a new set of dog from out map to pass into all the comparison methods
+//        The method I thought of
+//        Set<Dog> setOfDogs = new HashSet<>();
+//        for (Dog god : dogDataFromTxt.values()) {
+//            setOfDogs.add(god);
+//        }
+
+        // Javas first suggestion for optimization
+//        Set<Dog> setOfDogs = new HashSet<>();
+//        setOfDogs.addAll(dogDataFromTxt.values());
+
+        // Javas second suggestions for optimization
+        Set<Dog> setOfDogs = new HashSet<>(dogDataFromTxt.values());
+
+        // Assigning the users input of their dream dog to a new dog object to pass into our comparison methods
+        Dog usersDogRequest = ar.getUserInput();
+
+
+        // Matching the dog breeds from available dogs to user defined breed
+        System.out.println("Dogs with matching breed:");
+        Set<Dog> narrowedSet = ar.isBreedSame(setOfDogs, usersDogRequest);
+        for (Dog god : narrowedSet){
+            System.out.println(god.getName() + " " + god.getMicrochip() + " is " + god.age() + " years old. De-sexed:" +
+                    god.isDeSexed() + ". Sex: " + god.getSex());
+        }
+        // Matching the dog breeds and age ranges
+        System.out.println("Dogs with matching breed, within age range:");
+        narrowedSet = ar.isMatchAgeRange(narrowedSet, usersDogRequest.minAge, usersDogRequest.maxAge);
+        for (Dog god : narrowedSet){
+            System.out.println(god.getName() + " " + god.getMicrochip() + " is " + god.age() + " years old. De-sexed:" +
+                    god.isDeSexed() + ". Sex: " + god.getSex());
+        }
+        // Matching dog breeds, age ranges and sexes
+        System.out.println("\nDogs with matching breed, within age range with matching sex");
+        narrowedSet = ar.isSexSame(narrowedSet, usersDogRequest);
+        for (Dog god : narrowedSet){
+            System.out.println(god.getName() + " " + god.getMicrochip() + " is " + god.age() + " years old. De-sexed:" +
+                    god.isDeSexed() + ". Sex: " + god.getSex());
+        }
+        // Matching dog breeds, age ranges and sexes and desexed
+        System.out.println("\nDogs with matching all criteria");
+        narrowedSet = ar.isDeSexedSame(narrowedSet, usersDogRequest);
+        for (Dog god : narrowedSet){
+            System.out.println(god.getName() + " " + god.getMicrochip() + " is " + god.age() + " years old. De-sexed:" +
+                    god.isDeSexed() + ". Sex: " + god.getSex());
+        }
+    }
 
     /**
      * This Method reads a file of dog information: breed, name, chip number,etc and parses it into
@@ -107,10 +164,69 @@ public class AdoptionRequest {
         return new Dog(breed, sex, deSexed, minAge, maxAge);
     }
 
+    /**
+     * Compares aset of dogs passed in, to a user defined dog and returns a list of dogs that match the users
+     * dog if their breeds are the same
+     * @param dogList a set of dogs
+     * @param dog a user defined dog
+     * @return a new set of dogs that match the criteria
+     */
+    public Set<Dog> isBreedSame(Set<Dog> dogList, Dog dog) {
+        Set<Dog> newSetOfDogs = new HashSet<>();
 
+        for (Dog god : dogList) {
+            if (dog.compareBreed(god)) {newSetOfDogs.add(god);}
+        }
+        return newSetOfDogs;
+    }
+
+    /**
+     * Compares aset of dogs passed in, to a user defined dog and returns a list of dogs that match the users
+     * dog if their sex are the same
+     * @param dogList a set of dogs
+     * @param dog a user defined dog
+     * @return a new set of dogs that match the criteria
+     */
+    public Set<Dog> isSexSame(Set<Dog> dogList, Dog dog) {
+        Set<Dog> newSetOfDogs = new HashSet<>();
+
+        for (Dog god : dogList) {
+            if (dog.compareSex(god)) {newSetOfDogs.add(god);}
+        }
+        return newSetOfDogs;
+    }
+
+    /**
+     * Compares every dog in a list against a min and max age provided by the user to determine if any dogs
+     * that are available to adopt fall within the desired age range.
+     * @param dogList a list of dogs
+     * @param min an int for the minimum age of a dog
+     * @param max an int for the maximum age of at dog
+     * @return a new list of dogs theat meet the critea
+     */
+    public Set<Dog> isMatchAgeRange(Set<Dog> dogList, int min, int max) {
+        Set<Dog> newSetOfDogs = new HashSet<>();
+
+        for (Dog god : dogList) {
+            if (god.ageBetween(min, max)) {
+                newSetOfDogs.add(god);
+            }
+        }
+        return newSetOfDogs;
+    }
+
+    public Set<Dog> isDeSexedSame(Set<Dog> dogList, Dog dog){
+        Set<Dog> newSetOfDogs = new HashSet<>();
+
+        for(Dog god : dogList) {
+            if (dog.compareDeSexed(god)) {newSetOfDogs.add(god);}
+        }
+        return newSetOfDogs;
+    }
 
     private static void exitOnNull(){
         JOptionPane.showMessageDialog(null,"Sorry, have a nice day");
         System.exit(0);
     }
 }
+
--- src/Dog.java
+++ src/Dog.java
@@ -1,4 +1,4 @@
-
+// TODO - change strings male and female to M and F
 /**
  * Handles all of the needed information and methoods for our dogs.
  * @param args
@@ -10,6 +10,8 @@ public class Dog {
         private String sex;
         private boolean deSexed;
         private int age;
+        int minAge;
+        int maxAge;
 
         public Dog(String name, long microchip, String breed, String sex, boolean deSexed, int age) {
             this.name = name;
@@ -21,6 +23,11 @@ public class Dog {
         }
 
         public Dog(String breed, String sex, boolean deSexed, int minAge, int maxAge) {
+            this.breed = breed;
+            this.sex = sex;
+            this.deSexed = deSexed;
+            this.minAge = minAge;
+            this.maxAge = maxAge;
         }
 
     // Getters
@@ -31,17 +38,17 @@ public class Dog {
          * from outside the class at the moment.
          * @return
          */
-        private String getName(){ return name; }
+        public String getName(){ return name; }
 
-        private Long getMicrochip(){return microchip; }
+        public Long getMicrochip(){return microchip; }
 
-        private String getBreed(){return breed; }
+        public String getBreed(){return breed; }
 
-        private String getSex(){return sex; }
+        public String getSex(){return sex; }
 
         public boolean isDeSexed() {return deSexed; }
 
-        private int age() {return age; }
+        public int age() {return age; }
 
         // Setters
         public void setDeSexed(boolean deSexed) {
